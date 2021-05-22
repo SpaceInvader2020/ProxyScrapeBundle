6e7c6a459a886a326f0e0d2399c2f26b655d8512
Edvinas <30195711+SpaceInvader2020@users.noreply.github.com>
2021-05-22 10:49:05 +0100

    fixed client Factory

--- src/ProxyScrape/Factory.php
+++ src/ProxyScrape/Factory.php
@@ -2,13 +2,11 @@
 
 namespace PhpForce\ProxyScrapeBundle\ProxyScrape;
 
-use Psr\Http\Client\ClientInterface;
-
 class Factory
 {
     protected static string $clientClass = Client::class;
 
-    public static function create(string $auth, string $baseUrl, ClientInterface $httpClient): ClientInterface
+    public static function create(string $auth, string $baseUrl, \GuzzleHttp\ClientInterface $httpClient): ClientInterface
     {
         return new static::$clientClass($auth, $baseUrl, $httpClient);
     }
